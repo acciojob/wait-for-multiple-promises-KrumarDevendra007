7c89b0663f7215cc16fd2913ec04a675658090f8
Kumar Devendra <67205183+KrumarDevendra007@users.noreply.github.com>
2024-12-22 00:15:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+const table = document.getElementByClass("table");
+
+Promise.all([
+	new Promise((resolve, reject) => {
+		 const row1 = table.insertRow(0);
+		 const cell1 = table.insertCell(0);
+		 const cell2 = table.insertCell(1);
+
+		 cell1.innerHTML = "Loading..."
+		 if(scucess){
+			 ;
+			 
+		 }
+		
+	}),
+	new Promish((resolve, reject) => {}),
+	new Promise((resolve, reject) => {})
+]);
