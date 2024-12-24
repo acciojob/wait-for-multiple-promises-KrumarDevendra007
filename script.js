b70e75ff555054e0000edc8df7c6aceeb329e955
Kumar Devendra <67205183+KrumarDevendra007@users.noreply.github.com>
2024-12-24 21:46:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
 //your JS code here. If required.
 
-const table = document.getElementByClass("table");
+//funtion for time between 1 to 3
 
-Promise.all([
-	new Promise((resolve, reject) => {
-		 const row1 = table.insertRow(0);
-		 const cell1 = table.insertCell(0);
-		 const cell2 = table.insertCell(1);
+const createPromise = (name) => {
 
-		 cell1.innerHTML = "Loading..."
-		 if(scucess){
-			 ;
-			 
-		 }
-		
-	}),
-	new Promish((resolve, reject) => {}),
-	new Promise((resolve, reject) => {})
-]);
+	const time = Math.random() * 2+1;
+	return new Promise((resolve) => {
+		setTimeout(() => resolve({name, time:time.ToFixed(3)}), time*1000);
+	});
+}
+
+// array of three promises
+const promises = [
+	createPromise("Promise 1"),
+	createPromise("Promise 2"),
+	createPromise("Promise 3")
+];
+
+// Add "Loading..." row initially
+const output = document.getElementById("output");
+
+// Use Promise.all to wait for all promises to resolve
+const startTime = performance.now();
+promise.all(promises).then((results) => {
+	// Calculate total time taken
+	const totalTime = (performance.now() - startTime)/1000;
+
+	// Remove the "Loading..." row
+	document.getElementById("loading-row").remove();
+
+	// Add rows for each promise
+	results.forEach((results) => {
+		const row = document.createElement("tr");
+		row.innerHTML = `
+		   <td>${result.name}</td>
+		   <td>${result.time}</td>
+		`;
+		output.appendChild(row);	
+
+		 // Add row for the total time
+		const totalRow = document.createElement("");
+		totalRow.innerHTML = `
+		<td>Total</td>
+		<td>${totalTime.toFixed(3)} s</td>
+		`;
+		output.appendChild(totalRow);
+	})
+})
+
